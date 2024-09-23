6078cdbd76d816cff02edde05e3bada7f1f25fe5
YunGeun Jang <ygjang9120@gmail.com>
2024-09-23 13:38:43 +0900

    C:/Program Files/Git/gift 테스트2

--- app/gift/page.tsx
+++ app/gift/page.tsx
@@ -97,7 +97,7 @@ export default function GiftPage() {
         backgroundSize: "400% 400%",
         backgroundImage:
           "linear-gradient(288deg, rgba(26,46,91,100) 38%, rgba(60,132,206,1) 78%, rgba(3,180, 237,100) 88%, rgba(255,255,255,51) 99%)",
-        minHeight: "90vh",
+          height: "calc(var(--vh, 1vh) * 100)",
         overflow: "hidden"
         // padding: "1rem",
       }}
